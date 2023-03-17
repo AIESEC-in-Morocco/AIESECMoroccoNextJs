8e70ed16490e74fa640f3d71fddb4dce94162791
AymenMohamed-Ahmed <aymen.mo.ahmed1@gmail.com>
2023-03-17 20:51:49 +0100

    new

---
+++ src/components/AwsSlider/index.tsx
@@ -0,0 +1,86 @@
+import getImageFromGoogleDrive from "@/utils/getImageFromGoogleDrive";
+import Image from "next/image";
+import AwesomeSlider from "react-awesome-slider";
+import "react-awesome-slider/dist/styles.css";
+import AwesomeSliderStyles from "react-awesome-slider/src/styles";
+import AutoplaySlider from "react-awesome-slider/hoc/autoplay";
+// import AutoplaySlider from 'react-awesome-slider/hoc/autoplay';
+
+const images = [
+  {
+    id: 1,
+    alt: "Lions",
+    src: getImageFromGoogleDrive(
+      "https://drive.google.com/file/d/1Do-42ySiGjnesghP92zq8CWTzI0Me6Jk/view?usp=sharing"
+    ),
+  },
+  {
+    id: 2,
+    alt: "Beware_Hellala",
+    src: getImageFromGoogleDrive(
+      "https://drive.google.com/file/d/1E1o9iIrJ75n6Uj9J_HudVSVoiA1J31vH/view?usp=sharing"
+    ),
+  },
+  {
+    id: 3,
+    alt: "Mekcellence",
+    src: getImageFromGoogleDrive(
+      "https://drive.google.com/file/d/1CMG254iKE1tantX0OU_L_DyW30rDZbhl/view?usp=share_link"
+    ),
+  },
+  {
+    id: 4,
+    alt: "Wolves",
+    src: getImageFromGoogleDrive(
+      "https://drive.google.com/file/d/1F33Eb4w4Y-EQHnD5pzRT-LQx2y-MbhvV/view?usp=share_link"
+    ),
+  },
+  {
+    id: 5,
+    alt: "Gadirians",
+    src: getImageFromGoogleDrive(
+      "https://drive.google.com/file/d/1oFviRqm7qPaJPkyCi760w5-vbcUHVEDi/view?usp=share_link"
+    ),
+  },
+  {
+    id: 6,
+    alt: "Soldiers",
+    src: getImageFromGoogleDrive(
+      "https://drive.google.com/file/d/19F23h3nMN65_bYcH0ZqlHjUKkTmGgcAb/view?usp=share_link"
+    ),
+  },
+  {
+    id: 7,
+    alt: "Casawe_spirit",
+    src: getImageFromGoogleDrive(
+      "https://drive.google.com/file/d/1Smcx4BrMdNXsxCZYeS4-G3HrNF-yNfyT/view?usp=share_link"
+    ),
+  },
+  {
+    id: 8,
+    alt: "Fezberg",
+    src: getImageFromGoogleDrive(
+      "https://drive.google.com/file/d/1Vl0qvK6Ya5pP1OCMGOZZA-26QfMHvUK8/view?usp=share_link"
+    ),
+  },
+];
+
+const AutoplaySliderComponent = AutoplaySlider(AwesomeSlider);
+
+export const Slider = () => {
+  return (
+    <AutoplaySliderComponent
+      play={true}
+      cancelOnInteraction={false}
+      interval={3000}
+      cssModule={AwesomeSliderStyles}
+      animation="cubeAnimation"
+    >
+      {images.map((image) => (
+        <div key={image.id}>
+          <Image src={image.src} alt={image.alt} layout="fill" />
+        </div>
+      ))}
+    </AutoplaySliderComponent>
+  );
+};
